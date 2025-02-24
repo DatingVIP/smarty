14d7e7bf19157fc4a51c5645a8948b2b52a553a5
Boris Momčilović <boris.momcilovic@gmail.com>
2025-02-24 10:25:06 +0100

    nullable 8.4

--- libs/sysplugins/smarty_internal_templatebase.php
+++ libs/sysplugins/smarty_internal_templatebase.php
@@ -19,15 +19,15 @@
  * The following methods will be dynamically loaded by the extension handler when they are called.
  * They are located in a corresponding Smarty_Internal_Method_xxxx class
  *
- * @method Smarty_Internal_TemplateBase addAutoloadFilters(mixed $filters, string $type = null)
+ * @method Smarty_Internal_TemplateBase addAutoloadFilters(mixed $filters, ?string $type = null)
  * @method Smarty_Internal_TemplateBase addDefaultModifiers(mixed $modifiers)
  * @method Smarty_Internal_TemplateBase addLiterals(mixed $literals)
- * @method Smarty_Internal_TemplateBase createData(Smarty_Internal_Data $parent = null, string $name = null)
- * @method array getAutoloadFilters(string $type = null)
+ * @method Smarty_Internal_TemplateBase createData(?Smarty_Internal_Data $parent = null, ?string $name = null)
+ * @method array getAutoloadFilters(?string $type = null)
  * @method string getDebugTemplate()
  * @method array getDefaultModifier()
  * @method array getLiterals()
- * @method array getTags(mixed $template = null)
+ * @method array getTags(?mixed $template = null)
  * @method object getRegisteredObject(string $object_name)
  * @method Smarty_Internal_TemplateBase registerCacheResource(string $name, Smarty_CacheResource $resource_handler)
  * @method Smarty_Internal_TemplateBase registerClass(string $class_name, string $class_impl)
@@ -35,7 +35,7 @@
  * @method Smarty_Internal_TemplateBase registerDefaultPluginHandler(callback $callback)
  * @method Smarty_Internal_TemplateBase registerDefaultTemplateHandler(callback $callback)
  * @method Smarty_Internal_TemplateBase registerResource(string $name, mixed $resource_handler)
- * @method Smarty_Internal_TemplateBase setAutoloadFilters(mixed $filters, string $type = null)
+ * @method Smarty_Internal_TemplateBase setAutoloadFilters(mixed $filters, ?string $type = null)
  * @method Smarty_Internal_TemplateBase setDebugTemplate(string $tpl_name)
  * @method Smarty_Internal_TemplateBase setDefaultModifiers(mixed $modifiers)
  * @method Smarty_Internal_TemplateBase setLiterals(mixed $literals)
